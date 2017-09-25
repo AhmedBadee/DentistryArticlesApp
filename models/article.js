99ae22630d0aadc3352defbbd7acd4031858306b
AhmedBadee <ahmedbadee17@gmail.com>
2017-09-25 20:59:01 +0200

    DB Schema & Model

---
+++ models/article.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+
+const ArticleSchema = new Schema({
+    title: {
+        type: String,
+        required: [true, 'Title field is required']
+    },
+    brief: {
+        type: String,
+        required: [true, 'Brief field is required']
+    },
+    article: {
+        type: String,
+        required: [true, 'Article field is required']
+    }
+});
+
+const Article = mongoose.model('article', ArticleSchema);
+
+module.exports = Article;
