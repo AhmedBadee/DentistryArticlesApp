86ea0d128d75ba007c0b63c8d70555a1a4e1fd3b
AhmedBadee <ahmedbadee17@gmail.com>
2017-09-27 00:26:16 +0200

    receive any number of uploaded files

--- routes/api.js
+++ routes/api.js
@@ -1,21 +1,41 @@
 const express          = require('express');
 const Article          = require('../models/article');
-const bodyParser       = require('body-parser');
-const urlEncodedParser = bodyParser.urlencoded({extended: false});
+const multer           = require('multer');
 const router           = express.Router();
+
+const storage = multer.diskStorage({
+    destination: function(request, file, callback) {
+        callback(null, './images');
+    },
+    filename: function(request, file, callback) {
+        callback(null, request.body.title + file.originalname);
+    }
+});
+
+const upload = multer({storage: storage}).any();
 
 router.get('/article', function(request, response, next) {
     response.render('article');
 });
 
-router.post('/article', urlEncodedParser,function(request, response, next) {
-    Article.create({
-        title: request.body.title,
-        brief: request.body.brief,
-        article: request.body.article
-    }).then(function(article) {
-        response.send(article);
-    }).catch(next);
+router.post('/article', function(request, response, next) {
+    upload(request, response, function(error) {
+        if (error) throw error;
+
+        var uploadedFilesNames = [String];
+        for (var i = 0; i < request.files.length; i++) {
+            uploadedFilesNames[i] = request.files[i].filename;
+        }
+
+        Article.create({
+            title: request.body.title,
+            brief: request.body.brief,
+            article: request.body.article,
+            images: uploadedFilesNames
+        }).then(function(article) {
+            response.send(article);
+        }).catch(next);
+    });
 });
 
 router.put('/article/:id', function(request, response, next) {
