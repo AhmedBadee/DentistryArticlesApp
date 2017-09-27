53802e1edd9b78f200354408b36695357530e0e3
AhmedBadee <ahmedbadee17@gmail.com>
2017-09-28 00:31:39 +0200

    redirect to login page

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,10 @@
 const express          = require('express');
 const Article          = require('../models/article');
 const multer           = require('multer');
+
 const router           = express.Router();
+
+var session;
 
 const storage = multer.diskStorage({
     destination: function(request, file, callback) {
@@ -14,8 +17,31 @@ const storage = multer.diskStorage({
 
 const upload = multer({storage: storage}).any();
 
+router.get('/login', function(request, response) {
+    if (session) {
+        response.redirect('/article');
+    } else {
+        response.render('login');
+    }
+});
+
+router.post('/login', function(request, response) {
+    if (session) {
+        response.redirect('/article');
+    } else if (request.body.username == 'admin' && request.body.password == 'admin') {
+        request.sessionID = request.body.username;
+        session = request.sessionID;
+        response.redirect('/article');
+    }
+});
+
 router.get('/article', function(request, response, next) {
-    response.render('article');
+    if (session) {
+        response.render('article');
+        console.log(request);
+    } else {
+        response.redirect('/login');
+    }
 });
 
 router.post('/article', function(request, response, next) {
@@ -24,7 +50,7 @@ router.post('/article', function(request, response, next) {
 
         var uploadedFilesNames = [String];
         for (var i = 0; i < request.files.length; i++) {
-            uploadedFilesNames[i] = request.files[i].filename + 'fig' + (i + 1);
+            uploadedFilesNames[i] = request.files[i].filename + '_fig-' + (i + 1);
         }
 
         Article.create({
