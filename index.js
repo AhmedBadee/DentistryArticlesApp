196f3cabc531d90f1733aed99249ae18b448bcf4
AhmedBadee <ahmedbadee17@gmail.com>
2017-09-28 00:29:31 +0200

    added dependency express-session

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express    = require('express');
 const bodyParser = require('body-parser');
 const routes     = require('./routes/api');
 const mongoose   = require('mongoose');
+const session    = require('express-session');
 
 const app  = express();
 const statusUnprocessibleEntity = 422;
@@ -18,7 +19,15 @@ app.set('view engine', 'ejs');
 // handle static files
 app.use(express.static(__dirname + '/public'));
 
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.use(routes);
+
+app.use(session({
+    secret: '#@%%*&^(&^REDfgY)($@%23@!@#%%$%#^&',
+    resave: false,
+    saveUninitialized: true
+}));
 
 app.use(function(error, request, reponse, next) {
     reponse.status(statusUnprocessibleEntity).send({error: error.message});
