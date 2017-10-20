6654101e1014e112dc0f5bdd37dfe91af7450474
AhmedBadee <ahmedbadee17@gmail.com>
2017-10-20 03:03:39 +0200

    changed the added fields

--- public/add-entries.js
+++ public/add-entries.js
@@ -3,11 +3,15 @@ $(document).ready(function() {
     $('#add').click(function(e) {
         e.preventDefault();
 
-        $('#files').append('<div><input id="fileField" type="file" name="images[]" />' +
-                           '<input type="button" id="sub" value="-" /></div>');
+        $('#files').append('<div>' + 
+                                '<input class="image-box text-all urls" type="text" name="images[]" placeholder="Image URL" />' +
+                                '<div id="action-buttons">' +
+                                    '<input type="button" id="sub" value="-" />' +
+                                '</div>' +
+                            '</div>');
     });
 
     $('body').on('click', '#sub', function(e) {
-        $(this).parent('div').remove();
+        $(this).parent('div').parent('div').remove();
     });
 });
